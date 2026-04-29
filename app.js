16992e0ccc3fe33573d2e9bf50e8ea2bf2717f7c
DehBarca <dialebar@gmail.com>
2026-04-29 13:29:20 -0600

    Primer commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const moment = require("moment");
+let i = 5,
+  j = 10;
+console.log("Iniciando....");
+console.log("La suma es " + (i + j));
+
+let Fecha = moment().format("LL");
+console.log("Y la fecha es: ", Fecha);
